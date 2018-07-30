1acb9fc7b1ae2ae73f08b37d7e23f8568edfca18
cpcode <32060310+cpcode@users.noreply.github.com>
2018-07-30 16:50:42 +0800

    url去掉index.php
    config.php将index_page ='index.php'改为设置为空
    坏境需安装mod_write模块，否则出现404错误

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://xinhuaapi.app/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
